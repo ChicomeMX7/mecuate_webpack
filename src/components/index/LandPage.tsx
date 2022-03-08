0c74bb61ba28e7d8b16a06e2a4e4ca2dc50c11b5
JChicomeSan <57263663+ChicomeMX7@users.noreply.github.com>
2022-03-08 00:40:34 -0600

    Adding color variables in sass

--- src/components/index/LandPage.tsx
+++ src/components/index/LandPage.tsx
@@ -1,18 +1,16 @@
-///<reference  path="../../../src/typings/main.d.ts"/>
 import React from "react";
 import { ReactDOM } from "react";
 import "../../styles/main.sass";
 
-
-
-
-function LandPage(props:Mecuate.Q ): Mecuate.LandingPage {
+function LandPage(props: Mecuate.Props): Mecuate.LandPage {
 	let style_custom = `background:${props.color}`;
-	let nameOfPage = `name_of_page${props.name.replaceAll(" ", "_")}`;
+	let nameOfPage = `name_of_page${props.name ? props.name.replaceAll(" ", "_") : ""}`;
 	let rename = props.lol;
 	return (
 		<div id={nameOfPage}>
-			<h1>{props.name.toUpperCase()} for {rename}</h1>
+			<h1>
+				{props.name ? props.name.toUpperCase() : ""} for {rename}
+			</h1>
 			<h4 className="fiusha">Welcome to Landing Page for My Application</h4>
 			<hr />
 			<p>Test color is to be assigned:[{style_custom}]</p>
